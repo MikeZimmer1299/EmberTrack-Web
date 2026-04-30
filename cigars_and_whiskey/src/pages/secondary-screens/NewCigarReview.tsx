a3df2f0b1c533d9555b8f9be40a46bf10cb8acf3
Mike Zimmer <mjzimmer1121999@gmail.com>
2026-04-30 14:46:25 -0400

    Same as with the Android app, start with the most difficult screens first. NewCigarReview.tsx/.css added within secondary screens folder

---
+++ cigars_and_whiskey/src/pages/secondary-screens/NewCigarReview.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NewCigarReview.css";
+
+const NewCigarReview: React.FC = () => {
+    return (
+        <h1>New Cigar Review</h1>
+    )
+    
+}
+export default NewCigarReview;
